566fe5b5bc157fdb37a0248c5dc743b21489049e
drkrieger1 <eplyushko@gmail.com>
2018-02-26 00:03:41 -0800

    landing page ready to deploy

--- Portfolio/Portfolio/ClientApp/components/Footer.tsx
+++ Portfolio/Portfolio/ClientApp/components/Footer.tsx
@@ -5,12 +5,22 @@ export class Footer extends React.Component<{}, {}> {
     public render() {
         return <div>
             <div className='container-fluid footer'>
-                <div className='row'>                  
-                    <div className='col-sm-12 col-md-12 col-lg-12'>
-                        <p>This is the footer</p>
+                <div className="container">
+                    <div className="row justify-content-md-center footer-content">
+                        <div className="col-2 col-md-2 content2">
+                            <a className='social-link' href='https://github.com/drkrieger1'><i className="devicon-github-plain colored icon-social"></i></a>
+                            <a className='social-link' href='https://www.linkedin.com/in/erik-plyushko/'><i className="fa fa-linkedin-square icon-social"></i></a>
+                         </div>                       
                     </div>
                 </div>
-            </div>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-sm-12 col-md-12 col-lg-6 copyright'>
+                            <p>&copy; Copyright 2018 Erik Plyushko</p>
+                        </div>
+                    </div>
+                </div>
+            </div>         
         </div>
     }
 }
--- Portfolio/Portfolio/ClientApp/components/Home.tsx
+++ Portfolio/Portfolio/ClientApp/components/Home.tsx
@@ -6,22 +6,47 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
         return <div>
         <div className='jumbotron jumbotron-fluid jumbo'>
                 <div className='container'>
-                    <h1 className='display-4'>Full Stack Web Development</h1>
-                    <p className='lead'>This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                    <h1 className='display-4'>Full Stack Web Developer</h1>
+                    <p className='lead'>Providing modern web solutions</p>
                 </div>
         </div>
                 <div className='container-fluid'>
                     <div className='row'>
-                        <div className='col-sm-12 col-md-12 col-lg-6'>
+                    <div className='col-sm-12 col-md-12 col-lg-6 content'>
                         <h1>About</h1>
-                        <p>Mustache 8-bit green juice cliche humblebrag unicorn. Scenester hoodie
-                            wayfarers fingerstache, helvetica forage live-edge viral squid iceland
-                            crucifix copper mug. Godard yuccie forage chillwave craft beer. DIY salvia
-                            street art, affogato umami actually paleo keffiyeh 90's wolf gastropub selvage
-                            pop-up flannel.
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+                            placerat purus nec sapien tempus, a convallis erat rutrum. Pellentesque
+                            porttitor auctor urna, id sagittis velit pellentesque id. Nunc vitae diam
+                            ultricies, volutpat sapien a, rhoncus ante. Duis blandit libero sit amet justo
+                            rhoncus malesuada. Vivamus congue, nibh sit amet mattis lobortis, nunc odio auctor
+                            sapien, sit amet congue odio eros id elit. Donec at dignissim sem. Ut consequat ornare fermentum.
                         </p>
+                        <hr />
+                        <h1>Technologies</h1>                                        
+                    </div>
+                    </div>
+                </div>
+                <div className='container'>
+                    <div className='row'>
+                    <div className='col-6 col-md-2 content2'>
+                            <i className="devicon-csharp-plain colored icon"></i>
                         </div>
-                     </div>
+                    <div className='col-6 col-md-2 content2'>
+                            <i className="devicon-dot-net-plain colored icon"></i>
+                        </div>
+                    <div className='col-6 col-md-2 content2'>
+                            <i className="devicon-javascript-plain colored icon"></i>
+                        </div>
+                    <div className='col-6 col-md-2 content2'>
+                            <i className="devicon-react-original colored icon"></i>
+                        </div>
+                    <div className='col-6 col-md-2 content2'>
+                            <i className="devicon-css3-plain colored icon"></i>
+                        </div>
+                    <div className='col-6 col-md-2 content2'>
+                            <i className="devicon-html5-plain colored icon"></i>
+                        </div>                                  
+                    </div>
                 </div>
         </div>;
     }
