747adc11d2d2d9f12bec263da6eb9e5b8c0a58aa
drkrieger1 <eplyushko@gmail.com>
2018-02-26 23:53:03 -0800

    created component contact component

--- Portfolio/Portfolio/ClientApp/routes.tsx
+++ Portfolio/Portfolio/ClientApp/routes.tsx
@@ -2,11 +2,15 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
+import { Projects } from './components/Projects';
+import { Contact } from './components/Contact';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
 export const routes = <Layout>
-    <Route exact path='/' component={ Home } />
+    <Route exact path='/' component={Home} />
+    <Route path='/Projects' component={Projects} />
+    <Route path='/Contact' component={Contact} />
     <Route path='/counter' component={ Counter } />
     <Route path='/fetchdata' component={ FetchData } />
 </Layout>;
