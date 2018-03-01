e1a93d94e4dd881d554f8604bd7cbdda6b0f06e5
drkrieger1 <eplyushko@gmail.com>
2018-03-01 13:17:25 -0800

    projects added to projects page

--- Portfolio/Portfolio/ClientApp/components/Projects.tsx
+++ Portfolio/Portfolio/ClientApp/components/Projects.tsx
@@ -10,54 +10,54 @@ export class Projects extends React.Component<RouteComponentProps<{}>, {}> {
                 <div className='row top-buffer'>
                     <div className='col-sm-12 col-md-6 col-lg-4 mt-3 project-item'>
                         <div className='card'>
-                            <img className='card-img-top' src='http://via.placeholder.com/350x200' alt='Projects alt caption' />
+                            <img className='card-img-top' src={('/img/StickerFire.jpg')} alt='Image of StickerFire' />
                             <div className='card-block'>
-                                <p className='card-text ml-2'>Project caption</p>
+                                <p className='card-text ml-2'>StickerFire</p>
                             </div>
                         </div>
                     </div>
 
                     <div className='col-sm-12 col-md-6 col-lg-4 mt-3 project-item'>
                         <div className='card'>
-                            <img className='card-img-top' src='http://via.placeholder.com/350x200' alt='Projects alt caption' />
+                            <img className='card-img-top' src={('/img/Offshore.jpg')} alt='Image of Offshore' />
                             <div className='card-block'>
-                                <p className='card-text ml-2'>Project caption</p>
+                                <p className='card-text ml-2'>Offshore</p>
                             </div>
                         </div>
                     </div>
 
                     <div className='col-sm-12 col-md-6 col-lg-4 mt-3 project-item'>
                         <div className='card'>
-                            <img className='card-img-top' src='http://via.placeholder.com/350x200' alt='Projects alt caption' />
+                            <img className='card-img-top' src={('/img/Pacific.jpg')} alt='Image of Pacific Quality Construction' />
                             <div className='card-block'>
-                                <p className='card-text ml-2'>Project caption</p>
+                                <p className='card-text ml-2'>Pacific Quality Construction</p>
                             </div>
                         </div>
                     </div>
 
                     <div className='col-sm-12 col-md-6 col-lg-4 mt-3 project-item'>
                         <div className='card'>
-                            <img className='card-img-top' src='http://via.placeholder.com/350x200' alt='Projects alt caption' />
+                            <img className='card-img-top' src={('/img/Auto.jpg')} alt='Image of Auto Inspector' />
                             <div className='card-block'>
-                                <p className='card-text ml-2'>Project caption</p>
+                                <p className='card-text ml-2'>Auto Inspector</p>
                             </div>
                         </div>
                     </div>
 
                     <div className='col-sm-12 col-md-6 col-lg-4 mt-3 project-item'>
                         <div className='card'>
-                            <img className='card-img-top' src='http://via.placeholder.com/350x200' alt='Projects alt caption' />
+                            <img className='card-img-top' src={('/img/Gross.jpg')} alt='Image of Is It Gross' />
                             <div className='card-block'>
-                                <p className='card-text ml-2'>Project caption</p>
+                                <p className='card-text ml-2'>Is It Gross</p>
                             </div>
                         </div>
                     </div>
 
                     <div className='col-sm-12 col-md-6 col-lg-4 mt-3 project-item'>
                         <div className='card'>
-                            <img className='card-img-top' src='http://via.placeholder.com/350x200' alt='Projects alt caption' />
+                            <img className='card-img-top' src={('/img/DuckTimer.jpg')} alt='Image of Duck Timer' />
                             <div className='card-block'>
-                                <p className='card-text ml-2'>Project caption</p>
+                                <p className='card-text ml-2'>Duck Timer</p>
                             </div>
                         </div>
                     </div>
