9df16606f0b9f3d92ac93a7465fbd362de7a4429
drkrieger1 <eplyushko@gmail.com>
2018-03-01 17:33:27 -0800

    template code clean up

--- Portfolio/Portfolio/ClientApp/routes.tsx
+++ Portfolio/Portfolio/ClientApp/routes.tsx
@@ -4,13 +4,9 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { Projects } from './components/Projects';
 import { Contact } from './components/Contact';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 
 export const routes = <Layout>
     <Route exact path='/' component={Home} />
     <Route path='/Projects' component={Projects} />
     <Route path='/Contact' component={Contact} />
-    <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata' component={ FetchData } />
 </Layout>;
