e94faed5de40987d813dfd34e0d39937268ae42b
drkrieger1 <eplyushko@gmail.com>
2018-03-15 16:27:54 -0700

    new dev enviorment sync

--- Portfolio/Portfolio/Startup.cs
+++ Portfolio/Portfolio/Startup.cs
@@ -33,8 +33,8 @@ namespace Portfolio
                 app.UseDeveloperExceptionPage();
                 app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
                 {
-                    HotModuleReplacement = true,
-                    ReactHotModuleReplacement = true
+                    HotModuleReplacement = false,
+                    ReactHotModuleReplacement = false
                 });
             }
             else
